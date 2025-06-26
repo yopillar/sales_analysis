947004845d0ddc38887aec1da33008b9456f117c
yoanndonkerque <yoanndonkerque@gmail.com>
2025-06-26 13:54:43 +0200

    fix cart abandonment in mart query

--- dbt_sales_analysis/models/mart_product_perf.sql
+++ dbt_sales_analysis/models/mart_product_perf.sql
@@ -1,8 +1,18 @@
--- models/marts/mart_product_perf.sql  (complément)
+-- models/mart_product_perf.sql
 
-with funnel as (...),
-orders as (...),
-products as (...),
+{{ config(materialized='table') }}
+
+with funnel as (
+    select * from {{ ref('int_funnel') }}
+),
+
+orders as (
+    select * from {{ ref('stg_orders') }}
+),
+
+products as (
+    select * from {{ ref('stg_products') }}
+),
 
 joined as (
   select
@@ -15,9 +25,9 @@ joined as (
     round(sum(o.quantity * p.price), 2)        as ca_total,
     count(*) filter (where f.cart_ts is not null and f.purchase_ts is null) as nb_abandons
   from products p
-  left join funnel f on p.product_id = f.product_id
-  left join orders o on p.product_id = o.product_id
-  group by 1,2,3
+  left join funnel f on using(product_id)
+  left join orders o on using(product_id)
+  group by 1, 2, 3
 )
 
 select *,
